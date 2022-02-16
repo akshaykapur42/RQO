0508e035d0e6b31cf7fc5f4a6040c13d36a049dd
akapur <akapur@ramquest.com>
2022-02-16 14:27:02 -0600

    commit

---
+++ test/Migrations/002_jira1234.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="f9d98c4c-5865-4509-a044-89a62cc7310d" />
+GO
+
+PRINT N'Creating index [NonClusteredIndex-20220216-142511] on [dbo].[Abstract]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'NonClusteredIndex-20220216-142511' AND object_id = OBJECT_ID(N'[dbo].[Abstract]'))
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-20220216-142511] ON [dbo].[Abstract] ([County], [SurveyName])
+GO
